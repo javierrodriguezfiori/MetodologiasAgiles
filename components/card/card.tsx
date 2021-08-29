a456f22acfd5a0a351b9efb6200c2406d6efdeca
Pablo Miranda <pablo.miranda.alan95@gmail.com>
2021-08-29 10:51:50 -0300

    Fixed List

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -7,19 +7,18 @@ import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
 import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import { red } from '@material-ui/core/colors';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-import { Item } from '../../types/types';
+import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 345,
+    width:'25% ',
+    margin:20
   },
   media: {
     height: 0,
@@ -55,6 +54,7 @@ export default function ItemCard({url,titulo,descripcion}:props){
   };
 
   return (
+    <Grid container justify="center">
     <Card  className={classes.root}>
       <CardHeader
         title={titulo}
@@ -115,6 +115,7 @@ export default function ItemCard({url,titulo,descripcion}:props){
         </CardContent>
       </Collapse>
     </Card>
+    </Grid>
   );
 }
 
--- components/list/list.tsx
+++ components/list/list.tsx
@@ -1,26 +1,7 @@
-import { useEffect, useState } from "react";
-import { firestore } from "../../config";
 import Card from "../../components/card/card";
-import { Item } from "../../types/types";
-import { Grid } from "@material-ui/core";
 
-export default function CardList() {
-  const [comercios, setComercios] = useState([]);
-
-  useEffect(() => {
-    getComerciosFromFirestore();
-  }, []);
-
-  const getComerciosFromFirestore = async () => {
-      setComercios([]);
-    const querySnapshot = await firestore.collection("restaurants").get();
-    const docs = [];
-    querySnapshot.forEach((doc) => {
-        docs.push({...doc.data(),id:doc.id})
-    });
-    setComercios(docs);
-  };
-
+export default function CardList(props) {
+  const {comercios} = props;
   return (
     <>
       {comercios.map((rest) => (
